4268894a49087cdfd9e6b81e2fc796e0b615c95d
GongXian Cao <gongxiancao@hotmail.com>
2016-09-22 13:33:30 +0800

    add main code

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = function (stream, iterator, options) {
+  var batch = [];
+  var batchSize = options.batchSize;
+  return new Promise(function (resolve, reject) {
+    stream.on('data', function (doc) {
+      batch.push(doc);
+      if(batch.length < batchSize) {
+        return;
+      }
+      stream.pause();
+      var commit = batch;
+      batch = [];
+
+      Promise.map(commit, iterator, {concurrency: options.concurrency})
+        .then(function () {
+          stream.resume();
+          return null;
+        })
+        .catch(function (err) {
+          stream.destroy(err);
+          reject(err);
+        });
+      return null;
+    })
+    .on('error', reject)
+    .on('close', function () {
+      resolve();
+    });
+  })
+  .then(function () {
+    return Promise.map(batch, iterator, {concurrency: options.concurrency});
+  });
+};
